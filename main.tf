22b83cc2ea87bdc181c140fea2fa3a9d8bcc34c7
Jeff Spradlin <jeffmspradlin@gmail.com>
2023-09-12 09:13:46 -0500

    updating req ver

--- main.tf
+++ main.tf
@@ -4,3 +4,14 @@ resource "azurerm_resource_group" "rg01" {
   name     = "${var.env}-${var.rg.name}-01"
   location = var.rg.location
 }
+
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0, < 4.0"
+    }
+  }
+}
